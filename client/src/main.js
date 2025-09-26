ce2aba3a19944fb3fad9233ca9ac37b27a38a038
Aykin-Code <daniel.l.gray@outlook.com>
2025-09-26 10:45:22 +0100

    basic working start - ui needs doing

---
+++ client/src/main.js
@@ -0,0 +1,66 @@
+// this needs sorting, not displaying in the right order?
+const reviewDisplaySection = document.getElementById("app");
+
+// create a function to fetch data
+// fetch() function is asynchronous
+async function fetchReviews() {
+  // fetch() by default sends a GET request
+  const response = await fetch(`http://localhost:8080/reviews`);
+  const reviews = await response.json();
+  createReviews(reviews);
+}
+
+function createReviews(reviewsArray) {
+  reviewsArray.forEach((review) => {
+    const div = document.createElement("div");
+
+    console.log(review);
+    const nameElement = document.createElement("p");
+    const reviewElement = document.createElement("p");
+    const starratingElement = document.createElement("p");
+
+    // set the p element we made to have text from the review object
+    const nameElementElement = nameElement;
+    const reviewElementElement = reviewElement;
+    const starratingElementElement = starratingElement;
+
+    nameElementElement.innerText = review.name;
+    reviewElementElement.innerText = review.review;
+    // wrapping text and a variable in a string with so it reads x/5
+    starratingElementElement.innerText = `Rating: ${review.starrating} / 5`;
+
+    div.setAttribute("class", "reviews-container");
+    div.append(nameElement, reviewElement, starratingElement);
+    reviewDisplaySection.append(div);
+  });
+}
+
+// call the function so it runs
+fetchReviews();
+
+//  ===========================================
+const form = document.getElementById("form");
+
+form.addEventListener("submit", async (event) => {
+  event.preventDefault();
+
+  const data = new FormData(form);
+  const userReview = Object.fromEntries(data);
+  console.log(userReview);
+
+  // Now I need to send a POST request to my server
+  const response = await fetch(`http://localhost:8080/reviews`, {
+    headers: {
+      "Content-Type": "application/json",
+    },
+    method: "POST",
+    body: JSON.stringify(userReview),
+  });
+});
+
+// ===========================================
+
+// Populate content
+// nameElementElement.innerText = review.name;
+// reviewElementElement.innerText = review.review;
+// starratingElementElement.innerText = `Rating: ${review.starrating} / 5`;
---
+++ server/server.js
@@ -0,0 +1,75 @@
+// so many imports, so few tarrifs
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import pg from "pg";
+
+// config the env variables
+dotenv.config();
+
+// app and db setup
+const app = express();
+const db = new pg.Pool({
+  connectionString: process.env.DB_CONN,
+});
+
+// midleEarth?  no, just some middlewares
+app.use(express.json());
+app.use(cors());
+
+// routes, because we need 'em  (I gets it)
+app.get("/", (req, res) => {
+  res.status(200);
+  res.send("Top of the world to ya!");
+});
+
+// a bit of fun
+app.get("/Connor", (req, res) => {
+  res.send(
+    "I'm a lumberjack and I'm okay. I work all day and I <s>work</s> code all night!"
+  );
+});
+
+// admin messages
+app.get("/messages", (req, res) => {
+  res.send(
+    "<h1>Messages</h1><br>=======<br><br>No messges at present.<br><br>=======<br>End Of Line"
+  );
+});
+// ===========================================
+
+// ============================================
+// queriying our database is asynchronous.
+app.get("/reviews", async (req, res) => {
+  // fetch all reviews from sql table
+  const result = await db.query(`SELECT * FROM reviews`);
+
+  res.json(result.rows);
+});
+
+// post reviews to the database
+app.post("/reviews", async (req, res) => {
+  // When the client sends up infromation it is always in the request.body
+  const body = req.body;
+  const nameFromClient = req.body.name;
+  const reviewFromClient = req.body.review;
+  const starratingFromClient = req.body.starrating;
+  // debugging / checking
+  console.log(nameFromClient, reviewFromClient, starratingFromClient);
+
+  // we use $1, $2 as placeholders so we aren't just putting whatever sends us in the string. (read up on SQL injection).
+  // the second parameter is an array of values that will replace the placeholders in order.
+  // this is called a parameterized query.
+  const data = await db.query(
+    `INSERT INTO reviews (name, review, starrating) VALUES ($1, $2, $3)`,
+    [nameFromClient, reviewFromClient, starratingFromClient]
+  );
+
+  res.json({ status: "Opinions enteredS" }); // let the client know it worked,  (but where is it going to go?  dont see it in console.log)
+});
+
+// ============================================
+// is anybody there?
+app.listen(8080, () => {
+  console.log(`Server is running on port http://localhost:8080`);
+});
